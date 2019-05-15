c8c3e2320b84d8a9e916cf51dc85b350e7e6f3ae
Steven Hardy <steverhardy@gmail.com>
2019-05-15 14:24:07 -0400

    set up API calls

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,9 @@
 // all imports at top, import config to access api key
-import Config from '../../config.js'
+import Config from '../../config.js';
 
 
 // create an instance of the Config class
-let config = New Config();
+let config = new Config();
 
 
 // render the navbar into the header
@@ -14,7 +14,7 @@ $.get('../../components/header.html', function(res) {
 
 
 // write function to convert degrees
-
+function convertDegrees(deg_kelvin){return ((deg_kelvin - 273.15) * 9/5+32)}
 
 
 // create a function that logs the city entered in the form
@@ -24,6 +24,16 @@ function searchCity() {
 
   // TODO: add AJAX call to weather API and display info
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${config.getKey()}`;
+
+  $.get(url, function(res) {
+    console.log(res);
+
+    $('#city_name').text(`${res.name}, ${res.sys.country}`);
+    $('#high').html(`${convertDegrees(res.main.temp_max)} F`);
+    $('#low').html(`${convertDegrees(res.main.temp_min)} F`);
+    $('#humidity').text(`${res.main.humidity}%`)
+    $('#forecast').text(res.weather[0].description)
+  });
 
   $('#weather-info').css('display', 'block');
 }
