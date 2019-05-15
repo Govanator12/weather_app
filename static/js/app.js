28c003fe4a75b99d3392108b89fd45ca7c5f6d42
Steven Hardy <steverhardy@gmail.com>
2019-05-15 13:07:55 -0400

    initial commit

---
+++ static/js/app.js
@@ -0,0 +1,28 @@
+// all imports at top, import config to access api key
+
+
+
+// create an instance of the Config class
+
+
+
+// render the navbar into the header
+$.get('../../components/header.html', function(res) {
+  $('#nav').html(res)
+});
+
+
+
+// write function to convert degrees
+
+
+
+// create a function that logs the city entered in the form
+
+
+
+// check to see if the submit button is pressed, if it is, stop the event from refreshing the page, and call searchCity()
+
+
+
+// make search information not appear until they submit a city
